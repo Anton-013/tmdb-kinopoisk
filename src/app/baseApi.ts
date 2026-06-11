85adb9976ab8d3542d144bad3548bc326ff4e7fc
Anton-013 <antonchuvashovoren@mail.ru>
2026-06-11 13:29:51 +0300

    feat: add redux store, baseApi, and browser router

---
+++ src/app/baseApi.ts
@@ -0,0 +1,21 @@
+// import { handleError } from "@/common/utils"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const baseApi = createApi({
+    reducerPath: "api",
+    tagTypes: [],
+    keepUnusedDataFor: 5,
+    baseQuery: async (args, api, extraOptions) => {
+        const result = await fetchBaseQuery({
+            baseUrl: import.meta.env.VITE_BASE_URL,
+            headers: {
+                'API-KEY': import.meta.env.VITE_API_KEY,
+            },
+        })(args, api, extraOptions)
+
+        // handleError(api, result)
+
+        return result
+    },
+    endpoints: () => ({}),
+})
